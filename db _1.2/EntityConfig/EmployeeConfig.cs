280c92d47715c74e27ac45478b0e013f57e8e391
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 13:09:59 +0300

    added class EmployeeConfig

--- db _1.2/EntityConfig/EmployeeConfig.cs
+++ db _1.2/EntityConfig/EmployeeConfig.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using db__1._2.Entitites;
 
 namespace db__1._2.EntityConfig
 {
-    class EmployeeConfig
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
     {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.ToTable("Employee").HasKey(p => p.EmployeeId);
+            builder.Property(p => p.EmployeeId).HasColumnName("EmployeeId").IsRequired();
+            builder.Property(p => p.FirstName).HasColumnName("FirstName").HasMaxLength(50).IsRequired();
+            builder.Property(p => p.LastName).HasColumnName("LastName").HasMaxLength(50).IsRequired();
+            builder.Property(p => p.HiredDate).HasColumnName("HiredDate").IsRequired();
+            builder.Property(p => p.DateOfBirth).HasColumnName("DateOfBirth");
+
+            builder.HasOne(c => c.Office)
+                .WithMany(s => s.Employees)
+                .HasForeignKey(c => c.OfficeId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
