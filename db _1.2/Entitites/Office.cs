55c8650404253ccbff49a616f911eaec69854043
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 12:26:29 +0300

    added class Office

---
+++ db _1.2/Entitites/Office.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace db__1._2.Entitites
+{
+    public class Office
+    {
+        public int OfficeId { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
