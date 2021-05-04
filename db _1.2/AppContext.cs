4e856c7b147ba7d53d5c146cdb7e823e8b6afd19
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 13:25:12 +0300

    added class AppContext

---
+++ db _1.2/AppContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using db__1._2.Entitites;
+using db__1._2.EntityConfig;
+
+namespace db__1._2
+{
+    public class ApplicationsContext : DbContext
+    {
+        public ApplicationsContext(DbContextOptions<ApplicationsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeOfProject> EmployeeProjects { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeProjectConfiguration());
+            modelBuilder.ApplyConfiguration(new OfficeConfiguration());
+            modelBuilder.ApplyConfiguration(new ProjectConfiguration());
+            modelBuilder.ApplyConfiguration(new TitleConfiguration());
+            modelBuilder.ApplyConfiguration(new ClientConfig());
+        }
+    }
+}
