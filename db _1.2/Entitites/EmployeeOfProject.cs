6ffe45b611e6b35484e6524ded0fee0a44d4642c
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 12:19:24 +0300

    create class Employee

---
+++ db _1.2/Entitites/EmployeeOfProject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace db__1._2.Entitites
+{
+    public class EmployeeOfProject
+    {
+        public int EmployeeOfProjectId { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime DayOfStart { get; set; }
+        public Employee Employee { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}
