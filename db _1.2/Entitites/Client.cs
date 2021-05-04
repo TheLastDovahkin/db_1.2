bbe78dcc24e4203190b5b31fbf4f6f5f64abb7eb
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 12:12:40 +0300

    create class client

---
+++ db _1.2/Entitites/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace db__1._2.Entitites
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Company { get; set; }
+        public List<Project> Projects { get; set; } = new List<Project>();
+    }
+}
