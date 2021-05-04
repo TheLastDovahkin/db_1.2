7b3facd4606c0cd2a39301d71c9b3e0c56b68c2b
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 12:20:36 +0300

    added class employeeOfProject

---
+++ db _1.2/Entitites/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace db__1._2.Entitites
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HiredDate { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public int TitleId { get; set; }
+        public Title Title { get; set; }
+
+        public int OfficeId { get; set; }
+        public Office Office { get; set; }
+
+        public List<EmployeeOfProject> EmployeeProjects { get; set; } = new List<EmployeeOfProject>();
+    }
+}
