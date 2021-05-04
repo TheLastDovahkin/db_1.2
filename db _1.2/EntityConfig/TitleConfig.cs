8122d6c6ce7ecb5f448ecba5f4d7c60a93601e4e
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 13:22:37 +0300

    added class TitleConfig

---
+++ db _1.2/EntityConfig/TitleConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using db__1._2.Entitites;
+
+namespace db__1._2.EntityConfig
+{
+    public class TitleConfiguration : IEntityTypeConfiguration<Title>
+    {
+        public void Configure(EntityTypeBuilder<Title> builder)
+        {
+            builder.ToTable("Title").HasKey(p => p.TitleId);
+            builder.Property(p => p.TitleId).HasColumnName("TitleId").IsRequired();
+            builder.Property(p => p.NameOfTitle).HasColumnName("Name").HasMaxLength(50).IsRequired();
+
+            builder.HasMany(c => c.Employees)
+                .WithOne(w => w.Title)
+                .HasForeignKey(f => f.TitleId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
