914c8bae5be5fe12ed1ceacea1f15b0b91fb3850
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 12:12:56 +0300

    create class project

---
+++ db _1.2/Entitites/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace db__1._2.Entitites
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string NameOfProject { get; set; }
+        public decimal Budget { get; set; }
+        public DateTime InitialDate { get; set; }
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+        public List<>
+    }
+}
