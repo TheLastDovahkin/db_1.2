0c3bd4288806f830d993f4088ca2135a31c7ff47
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 13:19:55 +0300

    added class OfficeConfig

---
+++ db _1.2/EntityConfig/OfficeConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using db__1._2.Entitites;
+
+namespace db__1._2.EntityConfig
+{
+    public class OfficeConfiguration : IEntityTypeConfiguration<Office>
+    {
+        public void Configure(EntityTypeBuilder<Office> builder)
+        {
+            builder.ToTable("Office").HasKey(p => p.OfficeId);
+            builder.Property(p => p.OfficeId).HasColumnName("OfficeId").IsRequired();
+            builder.Property(p => p.Title).HasColumnName("Title").HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Location).HasColumnName("Location").HasMaxLength(100).IsRequired();
+        }
+    }
+}
