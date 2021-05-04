396b85dfcdec91e0c7c095c455e06e026e384a3f
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 13:11:00 +0300

    added ClientConfig

---
+++ db _1.2/EntityConfig/ClientConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
+using db__1._2.Entitites;
+
+namespace db__1._2.EntityConfig
+{
+    public class ClientConfig : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client>builder)
+        {
+            builder.ToTable("Client").HasKey(p => p.ClientId);
+            builder.Property(p => p.ClientId).HasColumnName("ClientId").IsRequired();
+            builder.Property(p => p.FirstName).HasColumnName("FirstName").HasMaxLength(50).IsRequired();
+            builder.Property(p => p.LastName).HasColumnName("LastName").HasMaxLength(50).IsRequired();
+            builder.Property(p => p.DateOfBirth).HasColumnName("DateOfBirth").IsRequired();
+            builder.Property(p => p.Company).HasColumnName("Company").IsRequired();
+
+            builder.HasMany(c => c.Projects)
+                .WithOne(s => s.Client)
+                .HasForeignKey(c => c.ClientId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
