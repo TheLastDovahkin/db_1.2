990e0bb07b4a625c3e974abfe4d5fc9556147c36
Dmitrii Raven <oguvan4ik@gmail.com>
2021-05-04 12:26:44 +0300

    added class Title

---
+++ db _1.2/Entitites/Title.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace db__1._2.Entitites
+{
+    public  class Title
+    {
+        public int TitleId { get; set; }
+        public string NameOfTitle { get; set; }
+
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
